26851ca85debe9dd644f97ce7fa6516a2ee9ca26
toshinto <lyvcheto@abv.bg>
2022-08-06 10:12:12 +0300

    added authentication

--- FrontEnd/CarsDealer/src/app/app.module.ts
+++ FrontEnd/CarsDealer/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarService } from './car.service';
 import { CarComponent } from './CarTest/car/car.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent
+    CarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [CarService],
+  providers: [CarService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ FrontEnd/CarsDealer/src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators  } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      'username': ['', [Validators.required]],
+      'password': ['', Validators.required]
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.authService.login(this.loginForm.value).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  get username(){
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+}
